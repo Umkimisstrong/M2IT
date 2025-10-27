3c8b6cf4dbdcdd3e125b74f9d32725bb468d165e
KIM SANG KI <ckacl0233@gmail.com>
2025-10-27 16:30:48 +0900

    test

---
+++ Apps/Rutinus/Views/BackButtonView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Rutinus.ViewModels;
+
+namespace Rutinus
+{
+    public partial class BackButtonView : ContentView
+    {
+
+
+        public BackButtonView()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.Navigation.PopAsync();
+        }
+
+    }
+}
