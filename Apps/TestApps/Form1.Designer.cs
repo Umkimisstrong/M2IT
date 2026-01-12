d05d1e874c6ff9e80998c937718b3adb04fc79ec
KIM SANG KI <ckacl0233@gmail.com>
2026-01-12 15:15:31 +0900

    docking 테스트 디자이너

---
+++ Apps/TestApps/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace TestApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlBase = new Panel();
+            panel2 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            panel14 = new Panel();
+            panel15 = new Panel();
+            button3 = new Button();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            button2 = new Button();
+            panel4 = new Panel();
+            panel6 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel5 = new Panel();
+            panel7 = new Panel();
+            button1 = new Button();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            pnlBase.SuspendLayout();
+            panel2.SuspendLayout();
+            panel12.SuspendLayout();
+            panel15.SuspendLayout();
+            panel8.SuspendLayout();
+            panel11.SuspendLayout();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel7.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlBase
+            // 
+            pnlBase.BackColor = SystemColors.ActiveCaption;
+            pnlBase.BorderStyle = BorderStyle.FixedSingle;
+            pnlBase.Controls.Add(panel2);
+            pnlBase.Controls.Add(panel1);
+            pnlBase.Dock = DockStyle.Fill;
+            pnlBase.Location = new Point(0, 0);
+            pnlBase.Name = "pnlBase";
+            pnlBase.Padding = new Padding(3);
+            pnlBase.Size = new Size(1184, 851);
+            pnlBase.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.RosyBrown;
+            panel2.Controls.Add(panel12);
+            panel2.Controls.Add(panel8);
+            panel2.Controls.Add(panel4);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(3, 84);
+            panel2.Name = "panel2";
+            panel2.Padding = new Padding(3);
+            panel2.Size = new Size(1176, 762);
+            panel2.TabIndex = 2;
+            // 
+            // panel12
+            // 
+            panel12.BackColor = Color.LightSalmon;
+            panel12.Controls.Add(panel13);
+            panel12.Controls.Add(panel14);
+            panel12.Controls.Add(panel15);
+            panel12.Dock = DockStyle.Fill;
+            panel12.Location = new Point(279, 3);
+            panel12.Name = "panel12";
+            panel12.Padding = new Padding(2);
+            panel12.Size = new Size(894, 756);
+            panel12.TabIndex = 9;
+            // 
+            // panel13
+            // 
+            panel13.BackColor = SystemColors.ButtonFace;
+            panel13.Dock = DockStyle.Fill;
+            panel13.Location = new Point(2, 50);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(890, 704);
+            panel13.TabIndex = 7;
+            // 
+            // panel14
+            // 
+            panel14.BackColor = SystemColors.ButtonFace;
+            panel14.Dock = DockStyle.Top;
+            panel14.Location = new Point(2, 26);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(890, 24);
+            panel14.TabIndex = 6;
+            // 
+            // panel15
+            // 
+            panel15.BackColor = SystemColors.ButtonFace;
+            panel15.Controls.Add(button3);
+            panel15.Dock = DockStyle.Top;
+            panel15.Location = new Point(2, 2);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(890, 24);
+            panel15.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.Location = new Point(838, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(49, 12);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.Orange;
+            panel8.Controls.Add(panel9);
+            panel8.Controls.Add(panel10);
+            panel8.Controls.Add(panel11);
+            panel8.Dock = DockStyle.Left;
+            panel8.Location = new Point(129, 3);
+            panel8.Name = "panel8";
+            panel8.Padding = new Padding(2);
+            panel8.Size = new Size(150, 756);
+            panel8.TabIndex = 8;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = SystemColors.ButtonFace;
+            panel9.Dock = DockStyle.Fill;
+            panel9.Location = new Point(2, 50);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(146, 704);
+            panel9.TabIndex = 7;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = SystemColors.ButtonFace;
+            panel10.Dock = DockStyle.Top;
+            panel10.Location = new Point(2, 26);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(146, 24);
+            panel10.TabIndex = 6;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = SystemColors.ButtonFace;
+            panel11.Controls.Add(button2);
+            panel11.Dock = DockStyle.Top;
+            panel11.Location = new Point(2, 2);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(146, 24);
+            panel11.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(45, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(49, 12);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.PeachPuff;
+            panel4.Controls.Add(panel6);
+            panel4.Controls.Add(panel5);
+            panel4.Controls.Add(panel7);
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(3, 3);
+            panel4.Name = "panel4";
+            panel4.Padding = new Padding(2);
+            panel4.Size = new Size(126, 756);
+            panel4.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonFace;
+            panel6.Controls.Add(dataGridView1);
+            panel6.Dock = DockStyle.Fill;
+            panel6.Location = new Point(2, 50);
+            panel6.Name = "panel6";
+            panel6.Padding = new Padding(3);
+            panel6.Size = new Size(122, 704);
+            panel6.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(116, 698);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.ButtonFace;
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(2, 26);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(122, 24);
+            panel5.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.ButtonFace;
+            panel7.Controls.Add(button1);
+            panel7.Dock = DockStyle.Top;
+            panel7.Location = new Point(2, 2);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(122, 24);
+            panel7.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(69, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(49, 12);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.HotTrack;
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(3);
+            panel1.Size = new Size(1176, 81);
+            panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonFace;
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(3, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1170, 75);
+            panel3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(1184, 851);
+            Controls.Add(pnlBase);
+            Name = "Form1";
+            Text = "Form1";
+            pnlBase.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel12.ResumeLayout(false);
+            panel15.ResumeLayout(false);
+            panel8.ResumeLayout(false);
+            panel11.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel7.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlBase;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel10;
+        private Panel panel11;
+        private Panel panel4;
+        private Panel panel6;
+        private Panel panel5;
+        private Panel panel7;
+        private Panel panel3;
+        private Panel panel12;
+        private Panel panel13;
+        private Panel panel14;
+        private Panel panel15;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Apps/TestApps/Form1.cs
@@ -0,0 +1,10 @@
+namespace TestApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
